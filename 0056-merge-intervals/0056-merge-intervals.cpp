1bb7ef09b158c03c0aebea5ce5dcfb49f9968a36
tanvi56463 <162892916+tanvi56463@users.noreply.github.com>
2024-05-04 19:14:08 +0530

    Time: 30 ms (19.95%), Space: 22.8 MB (42.46%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,23 +6,12 @@ public:
         vector<vector<int>> ans;
         
         for(int i =0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
                 
             }
-            for(int j= i+1;j<n;j++){
-                if(intervals[j][0]<=end) // overlapping intrvals
-                {
-                    end = max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
-                    
-            }
-            ans.push_back({start,end});
+            else{ans.back()[1]=max(ans.back()[1],intervals[i][1]);}
+        
         }
         return ans;
         
