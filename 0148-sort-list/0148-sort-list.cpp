c59f2a4ac49669600e9478515649d15b76875a89
tanvi56463 <162892916+tanvi56463@users.noreply.github.com>
2024-06-13 08:47:58 +0530

    Time: 143 ms (30.97%), Space: 73.4 MB (47.15%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode*fast=head->next;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode* merge(ListNode* left, ListNode* right) {
+        if(left==0) return right;
+        if(right==0) return left;
+        ListNode* ans = new ListNode(-1);
+        ListNode* mptr =ans;
+        while(left && right){ // till one of the list is not null
+            if(left->val <= right->val){
+                mptr->next=left;
+                mptr = left;
+                left = left->next;
+            }
+            else{
+                mptr->next=right;
+                mptr = right;
+                right = right->next;
+            }
+        }
+        
+        if(left){
+            mptr->next=left;
+            //mptr = left;
+            //left = left->next;
+        }
+        if(right){
+            mptr->next=right;
+            //mptr = right;
+            //left = right->next;
+        }
+          
+        return ans->next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(head==0 || head->next ==0){
+            return head;
+        }
+        // break LL in 2 halves using mid node
+        ListNode* mid = findMid(head);
+        ListNode* left=head;
+        ListNode* right=mid->next;
+        mid->next=NULL;
+        
+        //sort RE
+        left=sortList(left);
+        right = sortList(right);
+        
+        //merge both left LL and right LL
+        ListNode *mergedLL = merge(left,right);
+        return mergedLL;
+    }
+};
