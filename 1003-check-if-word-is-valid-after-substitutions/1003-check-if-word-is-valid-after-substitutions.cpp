64da850fa93898b91c744227090e6d4e543586ad
tanvi56463 <162892916+tanvi56463@users.noreply.github.com>
2024-06-21 08:31:02 +0530

    Time: 13 ms (55.09%), Space: 9.6 MB (65.27%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,16 +1,54 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        if(s.size()==0){
-            return true;
-        }
-        int fnd = s.find("abc");
-        if(fnd != string::npos){
-            //string mil gayi hai
-            string tl = s.substr(0,fnd);
-            string tr = s.substr(fnd+3,s.size());
-            return isValid(tl+tr);
-        }
-        return false;
-    }
-};
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         if(s.size()==0){
+//             return true;
+//         }
+//         int fnd = s.find("abc");
+//         if(fnd != string::npos){
+//             //string mil gayi hai
+//             string tl = s.substr(0,fnd);
+//             string tr = s.substr(fnd+3,s.size());
+//             return isValid(tl+tr);
+//         }
+//         return false;
+//     }
+// };
+
+
+ class Solution {
+ public:
+      bool isValid(string s) {
+          if(s[0] != 'a') return false;
+          stack<char> st;
+          for(char ch:s){
+              if(ch == 'a')
+                  st.push(ch);  
+              else if(ch == 'b'){
+                  if(!st.empty() && st.top() == 'a'){
+                      st.push(ch);
+                  }
+                  else{
+                      return false;
+                  }
+              }
+              else{
+                  //ch = 'c'
+                  //pop 'b' and 'a'
+                  if(!st.empty() && st.top()=='b'){
+                      st.pop();
+                      if(!st.empty()&& st.top()=='a'){
+                          st.pop();
+                      }
+                      else
+                          return false;                   
+                  }
+                  else
+                      return false; 
+                  
+
+              }
+          }
+          return st.empty();
+           }
+    };
